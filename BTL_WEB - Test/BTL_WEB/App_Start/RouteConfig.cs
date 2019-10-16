a8ffbd123c1fb1e460c0f46ea4db4e83d0e77e97
asdcxsd <nguyenmanhthien98@gmail.com>
2019-10-16 17:23:58 +0700

    thienupload giao dien admin

--- BTL_WEB - Test/BTL_WEB/App_Start/RouteConfig.cs
+++ BTL_WEB - Test/BTL_WEB/App_Start/RouteConfig.cs
@@ -14,9 +14,10 @@ namespace BTL_WEB
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                 "{controller}/{action}/{id}",
+                 new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "BTL_WEB.Controllers" }
             );
         }
     }
